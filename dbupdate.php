d430933c0496c4ce0e820625ee7cc737e575d831
Scheant <steven.chean02@gmail.com>
2022-02-13 04:41:55 -0800

    add this please

---
+++ dbupdate.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Untitled</title>
+</head>
+
+<body>
+<table>
+
+<tr>
+<th>ID</th>
+<th>Nama</th>
+<th>Alamat</th>
+<th>Jabatan</th>
+</tr>
+
+<?php
+
+$connect = mysqli_connect("172.19.0.3", "root", "sqlpass", "trucorp-backup");
+
+$sql = "SELECT count(*) FROM users";
+$result = $connect->query($sql);
+
+while($row=$result->fetch_assoc())
+{
+echo "Total users: ".$row["count(*)"];
+?>
+<tr>
+<td> <?= $row["ID"]; ?></td>
+<td> <?= $row["Nama"]; ?></td>
+<td> <?= $row["Alamat"]; ?></td>
+<td> <?= $row["Jabatan"]; ?></td>
+</tr>
+
+<?php
+}
+?>
+
+</body>
+</html>
